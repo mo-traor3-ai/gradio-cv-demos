093a7ef57eab40c9b3efd41858f7778457bb3651
mo-traor3-ai <mohamed@roboflow.com>
2024-03-10 22:21:21 -0500

    create gradio app; write README; include 2 sample images

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # gradio-cv-demos
+
 Computer Vision model demos with Gradio interfaces.
+
+The first example is a Gradio app for a Groundlight AI Detector.
+
+## What is Groundlight?
+
+Groundlight gives you the ability to answer questions about visual data (e.g images, or video feeds) using natural language queries. This allows you to create complex computer vision systems in just a few lines of Python code, the question that maps to your business value, and a stream of visual data.
+
+### Example queries
+
+1. *Use Case: Workplace Safety* - "Is the person in the image wearing both their hard hat and safety vest properly?"
+
+2. *Use Case: Manufacturing Quality Assurance* - "Is there a warning label present on the product?"
+
+3. *Use Case: Process Control & Safety* - "Is the machine control switch in the "Off" position?
+
+4. *Use Case: Process Control* - "Are there two screws included in the packaging?
+
+## Using the Groundlight Gradio Demo
+
+1. Navigate to your chosen working directory.
+
+2. Create and activate a virtual environment running a Python version between 3.8 and 3.12
+
+Python 3.10 example (Linux):
+
+```bash
+python3.10 -m venv groundlight
+
+source gradio/bin/activate
+```
+
+3. Install Gradio and Groundlight with PIP
+
+```bash
+pip install gradio
+```
+
+4. Run the App
+
+```bash
+python3 app.py
+```
+
+Click on the link that opens in your terminal, or copy and paste it in a browser window. The default link for the app is `http://127.0.0.1:7860` (running locally).
---
+++ app.py
@@ -0,0 +1,65 @@
+from groundlight import Groundlight
+from PIL.Image import Image
+import gradio as gr
+
+
+def detection_query(GROUNDLIGHT_TOKEN: str, det_name: str, pil_img: Image, det_query: str,
+                    conf_threshold: float, wait_sec: float, patience_sec: float, human_review: str):
+    
+    # this variable is useful after setting a system environment variable for GROUNDLIGHT_PPE
+    # GROUNDLIGHT_TOKEN is your Groundlight API key (accessible in "Api tokens")
+    # GROUNDLIGHT_TOKEN = os.environ.get('GROUNDLIGHT_PPE')
+
+    gl = Groundlight(api_token=GROUNDLIGHT_TOKEN)
+
+    detector = gl.get_or_create_detector(
+        name=det_name,#example: ppe
+        query=det_query,
+        confidence_threshold=conf_threshold#example: 0.7
+        )
+
+    image_query = gl.submit_image_query(
+        detector=detector,
+        image=pil_img,
+        wait=wait_sec,#example: 10.0
+        patience_time=patience_sec,#example: 10.0
+        confidence_threshold=conf_threshold,#example: 0.7
+        human_review=human_review#ALWAYS or NEVER
+        )
+
+    result_label = image_query.result.label
+    result_confidence = f"{image_query.result.confidence:.2%}"
+
+    return result_label, result_confidence
+
+
+interface = gr.Interface(
+    fn=detection_query,
+    inputs=[
+        gr.Textbox(max_lines=1, label="Groundlight API Token", info="Create/access token: https://app.groundlight.ai/reef/my-account/api-tokens",
+                   show_label=True, type="password"),
+        gr.Textbox(max_lines=1, label="Detector Name", info="Example: ppe", show_label=True, type="text"),
+        gr.Image(type="pil", label="Upload Image"),
+        gr.Textbox(value="Is the person in the image wearing both their hard hat and safety vest properly?",
+                   info="What you'd like your model to answer", lines=2, max_lines=4, label="Detector Query", show_label=True),
+        gr.Slider(minimum=0.10, maximum=0.99, value=0.70, step=0.01, label="Confidence Threshold",
+                  info="Confidence threshold for ML detection results."),
+        gr.Slider(minimum=1.0, maximum=60.0, value=10.0, step=1.0, label="Wait Time (seconds)",
+                  info="How long to poll (in seconds) for a confident answer. This is a client-side timeout."),
+        gr.Slider(minimum=1.0, maximum=60.0, value=10.0, step=1.0, label="Patience Time (seconds)",
+                  info="How long to wait (in seconds) for a confident answer for this image query."),
+        gr.Dropdown(choices=["ALWAYS", "NEVER"], value="ALWAYS", type="value", multiselect=False, label="Human Review for Query Results?",
+                    info="Whether to send the image for human query (human-in-the-loop verification), regardless of ML result.")
+    ],
+    outputs=[gr.Textbox(max_lines=1, label="Query Result (Label)", info="The answer to your image query (for the submitted image).",
+                        show_label=True, interactive=False, type="text", show_copy_button=True),
+             gr.Textbox(max_lines=1, label="Query Result (Confidence)",  info="The confidence for your query result (for the submitted image).",
+                        show_label=True, interactive=False, type="text", show_copy_button=True)
+    ],
+    title="Groundlight Python SDK: Single Image Detection Queries",
+    description="Computer Vision powered by Natural Language. Groundlight makes it simple to build reliable visual applications."
+)
+
+
+if __name__ == "__main__":
+    interface.launch()
